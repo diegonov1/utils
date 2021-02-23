0319472505603ce7e506b3e75d49c6c51e3d2f03
Дiegushko Хименес <dkhimenesguakhardo@edu.hse.ru>
2021-02-24 02:10:55 +0300

    added lenet5 architechture

---
+++ EfficientNets_pytorch.py
@@ -0,0 +1,93 @@
+import torch
+import torch.nn as nn
+from math import ceil
+
+base_model = [
+    # expand_ratio, channels, repeats, stride, kernel_size
+    [1, 16, 1, 1, 3],
+    [6, 24, 2, 2, 3],
+    [6, 40, 2, 2, 5],
+    [6, 80, 3, 2, 3],
+    [6, 112, 3, 1, 5],
+    [6, 192, 4, 2, 5],
+    [6, 320, 1, 1, 3]
+]
+
+pthi_values = {
+    # pthi_value, resolution, drop_rate
+    "b0": (0, 244, 0.2), # alpha (depth), beta (width), gamma (resolution)
+    "b1": (0.5, 240, 0.2),
+    "b2": (1, 260, 0.3),
+    "b3": (2, 300, 0.3),
+    "b4": (3, 380, 0.4),
+    "b5": (4, 456, 0.4),
+    "b6": (5, 528, 0.5),
+    "b7": (6, 600, 0.5)
+}
+
+class CNNBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding, groups=1):
+        super(CNNBlock, self).__init__()
+        self.cnn = nn.Conv2d(
+            in_channels,
+            out_channels,
+            kernel_size,
+            stride,
+            padding,
+            groups=groups,   # If we set group=1 as we did by default, then this is a normal conv \
+            bias=False       # if we set it to groups=in_channels, then it is a Depthwise convolution.
+        )
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.silu = nn.SiLU() # SiLU > Swish
+
+    def forward(self, x):
+        return self.silu(self.bn(self.cnn(x)))
+
+class SqueezeExcitation(nn.Module):
+    def __init__(self, in_channels, reduced_dim):
+        super(SqueezeExcitation, self).__init__()
+        self.se = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1), # C x H x W -> C x 1 x 1
+            nn.Conv2d(in_channels, reduced_dim, 1),
+            nn.SiLU(),
+            nn.Conv2d(reduced_dim, in_channels, 1),
+            nn.Sigmoid()
+        )
+        def forward(self, x):
+            return x * self.se(x)
+
+class InvertedResidualBlock(nn.Module):
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        kernel_size,
+        stride,
+        padding,
+        expand_ratio,
+        reduction=4, # squeeze excitation
+        survival_prob=0.8 # for stochastic depth
+    ):
+        super(InvertedResidualBlock, self).__init__()
+        self.survival_prob = 0.8
+        self.use_residual = in_channels == out_channels and stride == 1
+        hidden_dim = in_channels * expand_ratio
+        self.expand = in_channels != hidden_dim
+        reduced_dim = int(in_channels / reduction)
+
+        if self.expand:
+            self.expand_conv = CNNBlock(
+                in_channels, hidden_dim, kernel_size=3, stride=1, padding=1
+            )
+
+        self.conv = nn.Sequential(
+            CNNBlock(
+                hidden_dim, hidden_dim, kernel_size, stride, padding, groups=hidden_dim
+            ),
+            SqueezeExcitation(hidden_dim, reduced_dim),
+            nn.Conv2d
+        )
+
+
+class EfficientNet(nn.Module):
+    pass
